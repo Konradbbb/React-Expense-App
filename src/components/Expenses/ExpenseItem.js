c6d3d36ff0eb3ff44be22697ca2e048f9941a661
Konrad Baran <konradabaran30@gmail.com>
2022-01-09 13:46:05 +0100

    added components responsible for user input, updated state logic

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,34 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
+  const [title, setTitle] = useState(props.title);
+  // const [amount, setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+  };
+
+  // const changePriceClickHandler = () => {
+  //   setAmount(0);
+  // }
+
+
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price '>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={changePriceClickHandler}>Change price</button> */}
     </Card>
-  );
+  ); 
 }
 
 export default ExpenseItem;
